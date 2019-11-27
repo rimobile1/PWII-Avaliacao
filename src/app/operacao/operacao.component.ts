5bcadc34ea3a3b67341d24dda0228268e8e77571
rimobile1 <icaro.mo1234@gmail.com>
2019-11-27 20:17:32 -0300

    Avaliacao

---
+++ src/app/operacao/operacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'has-operacao',
+  templateUrl: './operacao.component.html',
+  styleUrls: ['./operacao.component.css']
+})
+export class OperacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
